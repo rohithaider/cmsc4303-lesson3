6c87b1ae9c212a26bc9c74246aa2dbc322bcecf7
Mohtashim <rohithaider@gmail.com>
2021-03-11 14:09:47 -0600

    lecture 39

--- lib/model/photomemo.dart
+++ lib/model/photomemo.dart
@@ -9,6 +9,16 @@ class PhotoMemo {
   List<dynamic> sharedWith; //list of email
   List<dynamic> imageLabels; //image identidfied my machine learning
 
+  // key for Firestore document
+  static const TITLE = 'title';
+  static const MEMO = 'memo';
+  static const CREATED_BY = 'createdBy';
+  static const PHOTO_URL = 'photoURL';
+  static const PHOTO_FILENAME = 'photoFilename';
+  static const TIMESTAMPE = 'timestamp';
+  static const SHARED_WITH = 'sharedWith';
+  static const IMAGE_LABELS = 'imageLabels';
+
   PhotoMemo({
     this.docId,
     this.createdBy,
@@ -22,6 +32,19 @@ class PhotoMemo {
   }) {
     this.sharedWith ??= [];
     this.imageLabels ??= [];
+  }
+//from dart object to firestore document
+  Map<String, dynamic> serialize() {
+    return <String, dynamic>{
+      TITLE: this.title,
+      CREATED_BY: this.createdBy,
+      MEMO: this.memo,
+      PHOTO_FILENAME: this.photoFileName,
+      PHOTO_URL: this.photoURL,
+      TIMESTAMPE: this.timestamp,
+      SHARED_WITH: this.sharedWith,
+      IMAGE_LABELS: this.imageLabels,
+    };
   }
 
   static String validateTitle(String value) {
