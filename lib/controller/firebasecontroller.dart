d57f59f86f441ed8dd85db387ff29bd1601fb9fa
Mohtashim <rohithaider@gmail.com>
2021-03-11 13:18:21 -0600

    upload photo

--- lib/controller/firebasecontroller.dart
+++ lib/controller/firebasecontroller.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:lesson3/model/constant.dart';
 
 class FirebaseController {
   static Future<User> signIn({@required String email, @required String password}) async {
@@ -13,5 +18,24 @@ class FirebaseController {
 
   static Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
+  }
+
+  static Future<Map<String, String>> uploadPhotoFile({
+    @required File photo,
+    String filename,
+    @required String uid,
+  }) async {
+    filename ??= '${Constant.PHOTOIMAGE_FOLDER}/$uid/${DateTime.now()}';
+    UploadTask task = FirebaseStorage.instance.ref(filename).putFile(photo);
+    task.snapshotEvents.listen((TaskSnapshot event) {
+      var progress = event.bytesTransferred / event.totalBytes;
+      print('=======$progress');
+    });
+    await task;
+    String downloadURL = await FirebaseStorage.instance.ref(filename).getDownloadURL();
+    return <String, String>{
+      Constant.ARG_DOWNLOADURL: downloadURL,
+      Constant.ARG_FILENAME: filename,
+    };
   }
 }
--- lib/model/constant.dart
+++ lib/model/constant.dart
@@ -4,4 +4,9 @@ class Constant {
 
   static const SRC_CAMERA = 'camera';
   static const SRC_GALLERY = 'gallery';
+
+  static const PHOTOIMAGE_FOLDER = 'photo_images';
+
+  static const ARG_DOWNLOADURL = 'downloadurl';
+  static const ARG_FILENAME = 'filename';
 }
