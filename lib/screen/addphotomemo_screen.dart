39be8122a4e5c53d3a1424a117ba597f18a525d9
Mohtashim <rohithaider@gmail.com>
2021-03-11 11:46:56 -0600

    add photo memo

--- lib/screen/addphotomemo_screen.dart
+++ lib/screen/addphotomemo_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lesson3/model/constant.dart';
@@ -13,6 +15,9 @@ class AddPhotoMemoScreen extends StatefulWidget {
 class _AddPhotoMemoState extends State<AddPhotoMemoScreen> {
   _Controller con;
   User user;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  File photo;
+
   @override
   void initState() {
     super.initState();
@@ -28,7 +33,55 @@ class _AddPhotoMemoState extends State<AddPhotoMemoScreen> {
       appBar: AppBar(
         title: Text('Add PhotoMemo'),
       ),
-      body: Text('add photomemo: ${user.email}'),
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.4,
+                child: photo == null
+                    ? Icon(
+                        Icons.photo_library,
+                        size: 300,
+                      )
+                    : Image.file(
+                        photo,
+                        fit: BoxFit.fill,
+                      ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Title',
+                ),
+                autocorrect: true,
+                validator: null,
+                onSaved: null,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Memo',
+                ),
+                autocorrect: true,
+                keyboardType: TextInputType.multiline,
+                maxLines: 6,
+                validator: null,
+                onSaved: null,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'SharedWith (comma seperated email list)',
+                ),
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                maxLines: 2,
+                validator: null,
+                onSaved: null,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
