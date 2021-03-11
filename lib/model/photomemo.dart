7481901ddc89831c743298231d2e7711b6eef5ba
Mohtashim <rohithaider@gmail.com>
2021-03-11 12:10:58 -0600

    validate and save photo memo

--- lib/model/photomemo.dart
+++ lib/model/photomemo.dart
@@ -23,4 +23,31 @@ class PhotoMemo {
     this.sharedWith ??= [];
     this.imageLabels ??= [];
   }
+
+  static String validateTitle(String value) {
+    if (value == null || value.length < 3)
+      return 'too short';
+    else
+      return null;
+  }
+
+  static String validateMemo(String value) {
+    if (value == null || value.length < 5)
+      return 'too short';
+    else
+      return null;
+  }
+
+  static String validateSharedWith(String value) {
+    if (value == null || value.trim().length == 0) return null;
+
+    List<String> emailList = value.split(RegExp('(,| )+')).map((e) => e.trim()).toList();
+    for (String email in emailList) {
+      if (email.contains('@') && email.contains('.'))
+        continue;
+      else
+        return 'Comma(,) or space seperated email list';
+    }
+    return null;
+  }
 }
--- lib/screen/addphotomemo_screen.dart
+++ lib/screen/addphotomemo_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lesson3/model/constant.dart';
+import 'package:lesson3/model/photomemo.dart';
 
 class AddPhotoMemoScreen extends StatefulWidget {
   static const routeName = '/addPhotoMemoScreen';
@@ -32,6 +33,7 @@ class _AddPhotoMemoState extends State<AddPhotoMemoScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add PhotoMemo'),
+        actions: [IconButton(icon: Icon(Icons.check), onPressed: con.save)],
       ),
       body: Form(
         key: formKey,
@@ -55,8 +57,8 @@ class _AddPhotoMemoState extends State<AddPhotoMemoScreen> {
                   hintText: 'Title',
                 ),
                 autocorrect: true,
-                validator: null,
-                onSaved: null,
+                validator: PhotoMemo.validateMemo,
+                onSaved: con.saveTitle,
               ),
               TextFormField(
                 decoration: InputDecoration(
@@ -65,8 +67,8 @@ class _AddPhotoMemoState extends State<AddPhotoMemoScreen> {
                 autocorrect: true,
                 keyboardType: TextInputType.multiline,
                 maxLines: 6,
-                validator: null,
-                onSaved: null,
+                validator: PhotoMemo.validateMemo,
+                onSaved: con.saveMemo,
               ),
               TextFormField(
                 decoration: InputDecoration(
@@ -75,8 +77,8 @@ class _AddPhotoMemoState extends State<AddPhotoMemoScreen> {
                 autocorrect: false,
                 keyboardType: TextInputType.emailAddress,
                 maxLines: 2,
-                validator: null,
-                onSaved: null,
+                validator: PhotoMemo.validateSharedWith,
+                onSaved: con.saveSharedWith,
               ),
             ],
           ),
@@ -89,4 +91,27 @@ class _AddPhotoMemoState extends State<AddPhotoMemoScreen> {
 class _Controller {
   _AddPhotoMemoState state;
   _Controller(this.state);
+  PhotoMemo tempMemo = PhotoMemo();
+
+  void save() {
+    if (!state.formKey.currentState.validate()) return;
+    state.formKey.currentState.save();
+    print('=========== ${tempMemo.title}');
+    print('=========== ${tempMemo.memo}');
+    print('=========== ${tempMemo.sharedWith}');
+  }
+
+  void saveTitle(String value) {
+    tempMemo.title = value;
+  }
+
+  void saveMemo(String value) {
+    tempMemo.memo = value;
+  }
+
+  void saveSharedWith(String value) {
+    if (value.trim().length != 0) {
+      tempMemo.sharedWith = value.split(RegExp('(,| )+')).map((e) => e.trim()).toList();
+    }
+  }
 }
